3aaa677c65a590fc3aba04a3ab6ffc61ee318ae0
Frank Cheng <ufo22940268@gmail.com>
2020-12-23 09:22:54 +0800

    Add luma bot channel.

--- constants.js
+++ constants.js
@@ -10,6 +10,8 @@ exports.LUMA_NAME = inDevOrTest ? devLuma : productionLuma;
 exports.COMPASS_OWNER = "UrbanCompass";
 exports.COMPASS_FRONTEND_REPO = "uc-frontend";
 
-const dev_slack_channels = ['C01HAE5R72P'];
-const production_slack_channels = [''];
-exports.SLACK_CHANNELS = inDevOrTest ? dev_slack_channels : production_slack_channels;
+let LUMA_BOT_CHANNEL = 'C01H9RJ4518';
+const LUMA_DEV_BOT_CHANNEL = 'C01HAE5R72P';
+const DEV_SLACK_CHANNELS = [LUMA_DEV_BOT_CHANNEL];
+const PRODUCTION_SLACK_CHANNELS = [LUMA_BOT_CHANNEL];
+exports.SLACK_CHANNELS = inDevOrTest ? DEV_SLACK_CHANNELS : PRODUCTION_SLACK_CHANNELS;
